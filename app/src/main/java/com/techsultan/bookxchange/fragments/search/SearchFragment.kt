46152ee72812216a77ba23f5dbf21df3649f2514
Awodire babajide samuel <rhymezx.code@gmail.com>
2023-12-16 15:07:11 +0100

    Added the required changes, please read the TODO()

--- app/src/main/java/com/techsultan/bookxchange/fragments/search/SearchFragment.kt
+++ app/src/main/java/com/techsultan/bookxchange/fragments/search/SearchFragment.kt
@@ -1,31 +1,21 @@
 package com.techsultan.bookxchange.fragments.search
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
+import android.view.KeyEvent
 import android.view.LayoutInflater
-import android.view.Menu
-import android.view.MenuInflater
-import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
-import androidx.core.view.MenuHost
-import androidx.core.view.MenuProvider
-import androidx.fragment.app.activityViewModels
-import androidx.lifecycle.Lifecycle
+import android.widget.TextView
+import androidx.core.widget.addTextChangedListener
+import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
-import com.google.android.material.search.SearchView
-import com.techsultan.bookxchange.R
 import com.techsultan.bookxchange.adapter.BookAdapter
 import com.techsultan.bookxchange.adapter.GoogleBooksAdapter
 import com.techsultan.bookxchange.databinding.FragmentSearchBinding
-import com.techsultan.bookxchange.repository.ChatRepository
 import com.techsultan.bookxchange.repository.SearchRepository
-import com.techsultan.bookxchange.viewmodel.ChatViewModel
-import com.techsultan.bookxchange.viewmodel.ChatViewModelFactory
 import com.techsultan.bookxchange.viewmodel.SearchViewModel
 import com.techsultan.bookxchange.viewmodel.SearchViewModelFactory
-import materialsearchview.MaterialSearchView
 
 
 class SearchFragment : Fragment() {
@@ -54,12 +44,11 @@ class SearchFragment : Fragment() {
         val viewModelFactory = SearchViewModelFactory(repository)
         searchViewModel = ViewModelProvider(this, viewModelFactory)[SearchViewModel::class.java]
 
-        suggestedBooksRecyclerView()
-        searchResultRecyclerView()
-        searchViewModel.fetchBooks()
-
         val searchBar = binding.searchBar
         val searchView = binding.searchView
+
+        suggestedBooksRecyclerView()
+        searchResultRecyclerView()
 
         searchView.setupWithSearchBar(searchBar)
 
@@ -78,16 +67,48 @@ class SearchFragment : Fragment() {
             }
         })*/
 
-        searchBar.onFocusChangeListener = View.OnFocusChangeListener { v, hasFocus ->
-            if (hasFocus) {
-                // Show search suggestions or trigger animation
-                val query = searchBar.text.toString()
-                searchViewModel.searchBooks(query)
-            } else {
-                // Clear search query or perform other actions
-                searchBar.clearText()
+//        searchBar.onFocusChangeListener = View.OnFocusChangeListener { v, hasFocus ->
+//            if (hasFocus) {
+//                // Show search suggestions or trigger animation
+//                val query = searchBar.text.toString()
+//                searchViewModel.searchBooks(query)
+//            } else {
+//                // Clear search query or perform other actions
+//                searchBar.clearText()
+//            }
+//        }
+
+        //TODO: you can also use TextChangeListener to get the text from the searchView in realtime,
+        // and filter your search, which is equivalent to the onQueryListener.
+        searchView
+            .editText
+            .addTextChangedListener (
+                beforeTextChanged = { text, start, count, after ->
+
+                },
+                onTextChanged = { text, start, before, count ->
+//                    searchViewModel.searchBooks(text.toString())
+                },
+                afterTextChanged = {
+
+                }
+            )
+
+       //TODO: This will be triggered when you click on search from your phone touch keyboard
+        searchView
+            .editText
+            .setOnEditorActionListener { v: TextView?, actionId: Int, event: KeyEvent? ->
+                searchBar.text = searchView.text
+                searchView.hide()
+                //TODO: add your query search here, your query method itself from the
+                // searchViewModel.searchBooks() is not working, fix it.
+                //TODO: tested it, with searchViewModel.fetchBooks(), and i got a list after my search
+                //searchViewModel.fetchBooks()
+                searchViewModel.searchBooks(searchView.text.toString())
+                false
             }
-        }
+
+//        searchViewModel.fetchBooks()
 
     }
 
@@ -102,7 +123,8 @@ class SearchFragment : Fragment() {
         googleBooksAdapter = GoogleBooksAdapter()
 
         binding.booksRv.apply {
-            layoutManager = LinearLayoutManager(requireContext(), LinearLayoutManager.HORIZONTAL, false)
+            layoutManager = LinearLayoutManager(requireContext(), LinearLayoutManager.HORIZONTAL,
+                false)
             adapter = googleBooksAdapter
 
            searchViewModel.books.observe(viewLifecycleOwner) { books ->
